0170fbaae48bdd8dc54abfd080549cbfc62bb340
William Clayton <wilverine7@gmail.com>
2023-04-10 12:26:18 -0600

    added models

--- Backend/Mummies/Mummies/Pages/Index.cshtml.cs
+++ Backend/Mummies/Mummies/Pages/Index.cshtml.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Mummies.Pages;
 
+[AllowAnonymous]
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
